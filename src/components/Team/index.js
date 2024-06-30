b2b9f460e0a05612527d54d139efd8bb040033c8
Arham Anees <arhamanees10@gmail.com>
2024-06-30 14:30:11 +0500

    initial commit

---
+++ src/components/Team/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Team.css";
+
+const Team = () => {
+  return (
+    <div className="team-container" id="team">
+      <h1 className="coming-soon">Coming Soon...</h1>
+    </div>
+  );
+};
+
+export default Team;
